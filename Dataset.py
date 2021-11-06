f719b691bd92100c0671c21fc0818de294a06625
jujulili888 <78644176+jujulili888@users.noreply.github.com>
2021-11-06 23:17:10 +0800

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -6,6 +6,8 @@
 # 好处：
 # 把数据和模型、参数分离，解耦合
 # 可以把一组数据复用给多个模型
+import numpy as np
+import pickle
 
 
 class Dataset():
@@ -28,35 +30,63 @@ class Dataset():
     # trainRatio 给训练集
     # validateRatio 给验证集
     # 1 - trainRatio - validateRatio 给 test
-    # 若这两者和小于等于1，则抛出异常
+    # 若这两者和大于等于1，则抛出异常
     # @param trainRatio: all划分给train的比例
     # @param validateRatio: all划分给validate的比例
     def divideData(self, trainRatio=0.6, validateRatio=0.2):
-        pass
+        try:
+            testRatio = 1-trainRatio-validateRatio
+            if testRatio<=0:
+                raise ValueError("错误的划分比例！")
+        except ValueError as e:
+            print("引发异常：",repr(e))
+            raise
+        data_size = self.allLabel.shape()[1]
+        train_size = data_size*trainRatio
+        val_size = data_size*validateRatio
+        idxs = np.random.permutation(data_size)
+        train_idx = idxs[:train_size]
+        val_idx = idxs[train_size:train_size+val_size]
+        test_idx = idxs[train_size+val_size:]
+        self.trainData = self.allData[:,train_idx]
+        self.trainlabel = self.allLabel[:,train_idx]
+        self.validateData = self.allData[:,val_idx]
+        self.validateLabel = self.allLabel[:,train_idx]
+        self.testData = self.allData[:,test_idx]
+        self.testLabel = self.allLabel[:,test_idx]
 
     # TODO: (包含 all train validate test 的 getter)
     # 把数据集的输入和标签放在列表输出
     # @return allSet: 下标0为输入，下标1为标签
     def getAllSet(self):
-        pass
+        return [[self.allData, self.allLabel], [self.trainData, self.trainLabel],
+                [self.validateData, self.validateLabel], [self.testData, self.testLabel]]
 
     # TODO: (包含 all train validate test 的 setter)
     # 把数据集的输入和标签放在列表进行赋值
     # @param allSet: 下标0为输入，下标1为标签
     def setAllSet(self, allSet):
-        pass
+        self.allData, self.allLabel = allSet[0]
+        self.trainData, self.trainLabel = allSet[1]
+        self.validateData, self.validateLabel = allSet[2]
+        self.testData, self.testLabel = allSet[3]
 
     # TODO:
     # 把自身数据集保存到文件(文件结构方便读取和保存就行)
     # @param path: 保存文件的路径名
     def saveDataset(self, path):
-        pass
+        with open(path, 'wb') as f:
+            pickle.dump([self.trainData, self.trainLabel, self.validateData,
+                         self.validateLabel, self.testData, self.testLabel], f)
+        print("test data saved to {}".format(path))
 
     # TODO:
     # 从已有文件读取数据，直接更改自身成员
     # @param path: 读取文件的路径名
     def loadDataset(self, path):
-        pass
+        f = open(path, 'rb')
+        (self.trainData, self.trainLabel, self.validateData, self.validateLabel, self.testData, self.testLabel)\
+            = pickle.load(f)
 
     # DEBUG:
     # 输出所有成员
