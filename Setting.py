cf685031dcd1618afad0168fa819a006ade0e8f6
Charisk <2553036255@qq.com>
2021-11-10 11:29:53 +0800

    Format Setting.py

--- Setting.py
+++ Setting.py
@@ -9,6 +9,7 @@
 # 可以把一组参数复用给多个模型
 import numpy as np
 
+
 class Setting():
     # UPDATE: 有任何可能用到的参数都可以往这里加
     # 输入一些预先定义的参数，主要传递命名参数
@@ -34,20 +35,19 @@ class Setting():
                 self.weight[l] = 0.1 * np.random.randn(layers[l], layers[l-1])
         elif initialize == 'uniform':
             for l in range(1, self.depth):
-                self.weight[l] = 0.1 * np.random.uniform(0,1,(layers[l], layers[l-1]))
+                self.weight[l] = 0.1 * np.random.uniform(0, 1, (layers[l], layers[l-1]))
         elif initialize == 'xavier':
             for l in range(1, self.depth):
                 bound = np.sqrt(6./(layers[l] + layers[l-1]))
-                self.weight[l] = np.random.uniform(-bound,bound, (layers[l],layers[l-1]))
-        else: #输入错误
+                self.weight[l] = np.random.uniform(-bound, bound, (layers[l], layers[l-1]))
+        else:  # 输入错误
             pass
-        
 
     # TODO:
     # 把自身设置保存到文件(文件结构方便读取和保存就行)
     # @param path: 保存文件的路径名
     def saveSetting(self, path):
-        np.savez(path,layers=self.layers, batch=self.batch, alpha=self.alpha, epoch=self.epoch, initialize=self.initialize)
+        np.savez(path, layers=self.layers, batch=self.batch, alpha=self.alpha, epoch=self.epoch, initialize=self.initialize)
         print("param saved to {}.npz".format(path))
 
     # TODO:
@@ -55,52 +55,50 @@ class Setting():
     # @param path: 读取文件的路径名
     def loadSetting(self, path):
         t = np.load(path)
-        self.layers=t['layers']
+        self.layers = t['layers']
         self.depth = len(self.layers)
-        self.batch =t['batch']
-        self.alpha =t['alpha']
-        self.epoch =t['epoch']
-        self.initialize=t['initialize']
-        initialize=t['initialize']
+        self.batch = t['batch']
+        self.alpha = t['alpha']
+        self.epoch = t['epoch']
+        self.initialize = t['initialize']
+        initialize = t['initialize']
 
         if initialize == 'normal':
             for l in range(1, self.depth):
                 self.weight[l] = 0.1 * np.random.randn(self.layers[l], self.layers[l-1])
         elif initialize == 'uniform':
             for l in range(1, self.depth):
-                self.weight[l] = 0.1 * np.random.uniform(0,1,(self.layers[l], self.layers[l-1]))
+                self.weight[l] = 0.1 * np.random.uniform(0, 1, (self.layers[l], self.layers[l-1]))
         elif initialize == 'xavier':
             for l in range(1, self.depth):
                 bound = np.sqrt(6./(self.layers[l] + self.layers[l-1]))
-                self.weight[l] = np.random.uniform(-bound,bound, (self.layers[l],self.layers[l-1]))
-        else: #输入错误
+                self.weight[l] = np.random.uniform(-bound, bound, (self.layers[l], self.layers[l-1]))
+        else:  # 输入错误
             pass
-
-
-    
 
     # DEBUG:
     # 输出所有成员
-    def ParamShow(self):        
-        print('layers =',self.layers)
-        print('depth =',self.depth)
-        print('batch =',self.batch)
-        print('alpha =',self.alpha)
-        print('epoch =',self.epoch)
-        print('initialize =',self.initialize)
+    def ParamShow(self):
+        print('layers =', self.layers)
+        print('depth =', self.depth)
+        print('batch =', self.batch)
+        print('alpha =', self.alpha)
+        print('epoch =', self.epoch)
+        print('initialize =', self.initialize)
+
 
 if __name__ == '__main__':
-    #给定初始化参数
-    layers = [100,100,10]
-    s1 = Setting(layers,50,0.1,200,'uniform')
+    # 给定初始化参数
+    layers = [100, 100, 10]
+    s1 = Setting(layers, 50, 0.1, 200, 'uniform')
     s1.ParamShow()
     s1.saveSetting('parmlist')
     print("-"*30)
-    #默认参数    
+    # 默认参数
     s2 = Setting()
     s2.ParamShow()
     print("-"*30)
-    #读取文件
+    # 读取文件
     s3 = Setting()
     s3.loadSetting('parmlist.npz')
     s3.ParamShow()
