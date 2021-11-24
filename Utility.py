696727a6afde800d5901572e6fcc33ec65d6438a
wangyao <3050740551@qq.com>
2021-11-24 15:57:45 +0800

    utility

--- Utility.py
+++ Utility.py
@@ -5,7 +5,10 @@ import numpy as np
 def hardlim(x):
     x = np.where(x > 0, 1, 0)
     return x
-
+# 【激活函数】硬极限函数 这个有三段 >0 1 =0 0 <0 -1
+def hardlims(x):
+    x = np.where(x > 0, 1, np.where(x == 0, 0, -1))
+    return x
 
 # 【激活函数】一些其他的激活函数
 def RENAME_THIS_ACTIVATION_FUNCTION(x):
@@ -14,7 +17,8 @@ def RENAME_THIS_ACTIVATION_FUNCTION(x):
 
 # 【损失函数】均方误差函数
 def meanSquareError(a, y):
-    pass
+    m = a.shape[1]
+    return np.sum(np.square(y-a))/m/2
 
 
 # 【损失函数】一些其他的损失函数
